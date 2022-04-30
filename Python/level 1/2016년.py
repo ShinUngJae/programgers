133e75a7a841c45724addccda2acdd9a8da1bfb3
WOONGSONVI <37182279+ShinUngJae@users.noreply.github.com>
2022-04-30 20:10:02 +0900

    Update 2016년.py

--- Python/level 1/2016년.py
+++ Python/level 1/2016년.py
@@ -5,3 +5,10 @@ def solution(a, b) :
     weekday_list = ['MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT', 'SUN']
     answer = weekday_list[day.weekday()]
     return answer
+
+
+# 윤년
+# 4의 배수의 년에는 2월 29일
+# 단, 100의 배수인 년에는 2월 28일
+# 단, 400의 배수이면 2월 29일
+
